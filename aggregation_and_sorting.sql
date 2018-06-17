8e5b460c21a1d315c2a715b9a6d85502f3f0e5d3
Vijay Kumar <kasuladevivijay@gmail.com>
2018-06-17 23:44:11 +0530

    added some sql queries

---
+++ aggregation_and_sorting.sql
@@ -0,0 +1,17 @@
+﻿SELECT customers.first_name, customers.last_name, COUNT(purchases.id)
+FROM customers
+LEFT JOIN purchases
+ON customers.id = purchases.id
+GROUP BY customers.id
+
+SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
+INNER JOIN purchases
+ON items.id = purchases.id
+RIGHT JOIN customers
+ON purchases.customer_id = customers.id
+GROUP BY customers.id
+ORDER BY total_spent DESC
+
+SELECT * FROM customers 
+LEFT JOIN purchases
+ON customers.id = purchases.customer_id
